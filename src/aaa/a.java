7b45fe6aedbf3630cc93dcb593fc0d885fdeeb9c
myouj <18565544225@163.com>
2018-08-22 16:35:53 +0800

    合并冲突

--- src/aaa/a.java
+++ src/aaa/a.java
@@ -5,7 +5,7 @@ public class a {
 	public static void main(String[] args) {
 		System.out.println(1);
 		System.out.println(2);
-		System.out.println(6666666);
+		System.out.println(6666666+4);
 	}
 
 }
